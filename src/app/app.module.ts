7616968b3d01f6a57bd446ce54ff936a1ddfa8f3
Laptop E495 <sebastiancarv@gmail.com>
2021-10-29 22:33:22 -0500

    frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { DashboardComponent } from './componentes/dashboard/dashboard.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { CrearCuentaComponent } from './componentes/crear-cuenta/crear-cuenta.component';
+import { ModificarCuentaComponent } from './componentes/modificar-cuenta/modificar-cuenta.component';
+import { CrearUserComponent } from './componentes/crear-user/crear-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    DashboardComponent,
+    FooterComponent,
+    CrearCuentaComponent,
+    ModificarCuentaComponent,
+    CrearUserComponent
   ],
   imports: [
     BrowserModule,
